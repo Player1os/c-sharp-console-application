43f6eb743c8aa9f81f991e1c421ecfa543397f78
Osama Hassanein <player1os@gmail.com>
2018-03-08 09:08:41 +0100

    Local changes

---
+++ src/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace CSharpConsoleApplication {
+	class Program {
+		static void Main(string[] args) {
+			using (SQLiteConnection conn = new SQLiteConnection("URI=file:D:\\Projects\\Personal\\c-sharp-console-application\\Cookies")) {
+				Console.WriteLine("a");
+
+				int i = 0;
+
+				conn.Open();
+
+				Console.WriteLine("b");
+
+				var command = conn.CreateCommand();
+
+				command.CommandText = "SELECT name, encrypted_value FROM cookies;";
+
+				var reader = command.ExecuteReader();
+
+				Console.WriteLine("c");
+
+				while (reader.Read()) {
+					
+					var encryptedData = (byte[])reader[1];
+					var decodedData = System.Security.Cryptography.ProtectedData.Unprotect(encryptedData, null, System.Security.Cryptography.DataProtectionScope.CurrentUser);
+					var plainText = System.Text.Encoding.ASCII.GetString(decodedData); // Looks like ASCII
+					var clickId = reader.GetString(0);
+
+					Console.WriteLine(reader.GetString(0) + /*" " + reader.GetString(1) + */ " " + plainText);
+					++i;
+				}
+
+				Console.WriteLine("d");
+
+				conn.Close();
+
+				Console.WriteLine("eeee" + i.ToString());
+			}
+			Console.WriteLine("Hello World");
+		}
+	}
+}
